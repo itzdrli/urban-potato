206a6957413bfaf3dc29167c1ac07c4a8a0f3764
Itz_Dr_Li <liletian1231@outlook.com>
2023-01-03 20:59:15 +0000

    更新ip解析模块（雾

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,35 @@ use std::thread::sleep;
 use std::time::Duration;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::net::TcpStream;
+use std::time::Instant;
 
 fn main() {
-    println!("输入目标IP地址/域名（不带端口）：");
+    println!("输入目标IP地址/域名");
+    println!("示例: hypixel.net ");
     let mut address = String::new();
+    io::stdin().read_line(&mut address).expect("出现错误");
 
-    println!("请输入目标端口：");
-    let mut  port = String::new();
+    println!("请输入目标端口");
+    let mut port = String::new();
+    io::stdin().read_line(&mut address).expect("出现错误");
 
     println!("解析中...请稍后");
+    let start = Instant::now();
+    let stream = match TcpStream::connect(format!("baidu.com:80")) {
+        Ok(stream) => {
+            sleep(Duration::from_secs(1));
+            println!("解析成功!");
+            stream;
+        }
+        Err(e) => {
+            println!("在连接时出现错误: {}", e);
+            println!("程序将在5秒后自动退出");
+            sleep(Duration::from_secs(5));
+            std::process::exit(0);
+        }
+    };
 
-    println!("成功!");
 
     let proxies = rand::thread_rng().gen_range(860..=2023);
 
